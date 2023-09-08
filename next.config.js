40158c5a9a099086446459cd038a5619b2226a54
anas599 <anas599@yahoo.com>
2023-09-08 23:08:04 +0300

    add allowed image domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['olimpsport.com', 'upload.wikimedia.org'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'olimpsport.com',
+        port: '',
+        pathname: '/eu/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
