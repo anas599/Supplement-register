933da38ba87bc8c3a5359a3db09215a80c587faf
anas599 <anas599@yahoo.com>
2023-09-09 23:53:53 +0300

    add counter for items in the invoice

--- app/component/invoice.tsx
+++ app/component/invoice.tsx
@@ -1,5 +1,20 @@
 const Invoice = ({ selectedProducts }: any) => {
+  const calculateItemCount = (products: any, id: any) => {
+    return products.filter((product: any) => product.id === id).length;
+  };
+  const uniqueItemsMap = new Map();
+
+  // Populate the map with unique items and counts
+  selectedProducts.forEach((item: any) => {
+    const itemId = item.id;
+    if (uniqueItemsMap.has(itemId)) {
+      uniqueItemsMap.set(itemId, uniqueItemsMap.get(itemId) + 1);
+    } else {
+      uniqueItemsMap.set(itemId, 1);
+    }
+  });
   return (
+    // console.log(selectedProducts),
     <section className="invoice">
       <h2 className="text-center pb-1 mb-1">Invoice</h2>
 
@@ -20,30 +35,51 @@ const Invoice = ({ selectedProducts }: any) => {
                 Price
               </th>
               <th scope="col" className="px-6 py-3">
-                Action
+                Count
               </th>
             </tr>
           </thead>
           <tbody>
-            {selectedProducts.map((item: any, index: any) => (
+            {[...uniqueItemsMap.keys()].map((itemId, index) => (
               <>
-                <li className="list-none" key={index}></li>
+                <thead className="list-none" key={index}></thead>
                 <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
                   <th
                     scope="row"
                     className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                   >
-                    {item.name}
+                    {
+                      selectedProducts.find((item: any) => item.id === itemId)
+                        .name
+                    }
                   </th>
-                  <td className="px-6 py-4">{item.size}</td>
-                  <td className="px-6 py-4">{item.flavor}</td>
-                  <td className="px-6 py-4">${item.price}</td>
+                  <td className="px-6 py-4">
+                    {' '}
+                    {
+                      selectedProducts.find((item: any) => item.id === itemId)
+                        .size
+                    }
+                  </td>
+                  <td className="px-6 py-4">
+                    {' '}
+                    {
+                      selectedProducts.find((item: any) => item.id === itemId)
+                        .flavor
+                    }
+                  </td>
+                  <td className="px-6 py-4">
+                    $
+                    {
+                      selectedProducts.find((item: any) => item.id === itemId)
+                        .price
+                    }
+                  </td>
                   <td className="px-6 py-4">
                     <a
                       href="#"
                       className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
                     >
-                      Edit
+                      {uniqueItemsMap.get(itemId)}
                     </a>
                   </td>
                 </tr>
@@ -57,8 +93,11 @@ const Invoice = ({ selectedProducts }: any) => {
           <tr>
             <td colSpan={3}>Total:</td>
             <td>
-              {/* add key to total  */}$
-              {selectedProducts.reduce((total, item) => total + item.price, 0)}
+              $
+              {selectedProducts.reduce(
+                (total: any, item: any) => total + item.price,
+                0,
+              )}
             </td>
           </tr>
         </thead>
