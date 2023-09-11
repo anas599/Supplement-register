f25fc37e25473f2d564a7b8b81ac3867dfe21373
anas599 <anas599@yahoo.com>
2023-09-11 13:22:51 +0300

    update navbar to have logo and date

--- app/component/navBar.tsx
+++ app/component/navBar.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
+import Image from 'next/image';
 import {
   Bars3Icon,
   BellIcon,
@@ -13,6 +14,7 @@ import {
   ClipboardDocumentCheckIcon,
   CalendarDaysIcon,
 } from '@heroicons/react/24/outline';
+import formatDate from '../functions/formatDate';
 
 const navigation = [
   { name: 'Sale', href: '/', current: true, icon: CurrencyDollarIcon },
@@ -52,10 +54,12 @@ export default function NavBar() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
+                  <Image
+                    className="h-8 w-8 rounded-full"
+                    src="/registerIcon1.png"
+                    alt=""
+                    width={120}
+                    height={120}
                   />
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
@@ -83,7 +87,9 @@ export default function NavBar() {
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              {formatDate(new Date().toLocaleString() + '')}
+
+              {/* <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
                   type="button"
                   className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -93,16 +99,17 @@ export default function NavBar() {
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
 
-                {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
                     <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
-                      <img
+                      <Image
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src="/registerIcon.png"
                         alt=""
+                        width={200}
+                        height={60}
                       />
                     </Menu.Button>
                   </div>
@@ -158,7 +165,7 @@ export default function NavBar() {
                     </Menu.Items>
                   </Transition>
                 </Menu>
-              </div>
+              </div> */}
             </div>
           </div>
 
