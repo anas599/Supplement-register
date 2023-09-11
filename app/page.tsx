669cec8f2171bd462d80d6d8aca623695b0556b4
anas599 <anas599@yahoo.com>
2023-09-11 13:24:11 +0300

    remove unused code from main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,23 @@
 'use client';
-import Image from 'next/image';
-import { Fragment, useState, useEffect } from 'react';
-import Sell from './component/sell';
 import Invoice from './component/invoice';
 import ItemPick from './component/sellItemPick';
+import { useGlobalContext } from './context/store';
 export default function Home() {
-  const [selectedProducts, setSelectedProducts] = useState<any[]>([]);
+  const { selectedProducts, setSelectedProducts } = useGlobalContext();
   const addToInvoice = (product: any) => {
     setSelectedProducts([...selectedProducts, product]);
   };
+
   return (
-    <>
-      <section>
-        <div className="grid grid-cols-3 gap-1">
-          <div className="col-span-2 bg-gray-800 p-4 flex flex-wrap overflow-auto h-screen">
-            <ItemPick addToInvoice={addToInvoice} />
-          </div>
-          <div className="col-span-1 bg-gray-900 p-4 overflow-auto h-screen w-fit">
-            <Invoice selectedProducts={selectedProducts} />
-          </div>
+    <section>
+      <div className="grid grid-cols-3 gap-1">
+        <div className="col-span-2 bg-gray-800 p-4 flex flex-wrap overflow-auto h-screen">
+          <ItemPick addToInvoice={addToInvoice} />
         </div>
-      </section>
-    </>
+        <div className="col-span-1 bg-gray-900 p-4 overflow-auto h-screen w-auto">
+          <Invoice selectedProducts={selectedProducts} />
+        </div>
+      </div>
+    </section>
   );
 }
