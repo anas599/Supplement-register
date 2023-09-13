c12c68ac4385989cf50dd147a0ec123c26e4db14
anas599 <anas599@yahoo.com>
2023-09-13 10:00:00 +0300

    remove unused code

--- app/addItem/page.tsx
+++ app/addItem/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
 import dateISO8601 from '../functions/dateiso-8601';
-import handle from '@/pages/api/inventory';
 import InputProp from '../component/inputProp';
-import { tr } from 'date-fns/locale';
 const AddForm = () => {
   const [name, setName] = useState('');
   const [brand, setBrand] = useState('');
@@ -25,7 +23,7 @@ const AddForm = () => {
       quantity: parseInt(quantity),
       flavor: flavor,
       expiry: dateISO8601(expiryDate),
-      picture: picture,
+      // picture: 'picture',
       author: 1, // assuming the author is a user with id 1
     };
 
@@ -54,87 +52,96 @@ const AddForm = () => {
       console.error('An error occurred while posting', e);
     }
   };
+  const dataArray = [
+    {
+      name: 'name',
+      value: name,
+      label: 'Name',
+      onChange: (e: any) => setName(e.target.value),
+      type: 'string',
+      required: true,
+    },
+    {
+      name: 'brand',
+      value: brand,
+      label: 'Brand',
+      onChange: (e: any) => setBrand(e.target.value),
+      type: 'string',
+      required: true,
+    },
+    {
+      name: 'price',
+      value: price,
+      label: 'Price',
+      onChange: (e: any) => setPrice(e.target.value),
+      type: 'number',
+      required: true,
+    },
+    {
+      name: 'size',
+      value: size,
+      label: 'Size',
+      onChange: (e: any) => setSize(e.target.value),
+      type: 'string',
+      required: true,
+    },
+    {
+      name: 'quantity',
+      value: quantity,
+      label: 'Quantity',
+      onChange: (e: any) => setQuantity(e.target.value),
+      type: 'number',
+      required: true,
+    },
+    {
+      name: 'flavor',
+      value: flavor,
+      label: 'Flavor',
+      onChange: (e: any) => setFlavor(e.target.value),
+      type: 'string',
+      required: true,
+    },
+    {
+      name: 'expiryDate',
+      value: expiryDate,
+      label: 'Expiry Date',
+      onChange: (e: any) => setExpiryDate(e.target.value),
+      type: 'date',
+      required: true,
+    },
+    {
+      name: 'picture',
+      value: picture,
+      label: 'Picture',
+      onChange: (e: any) => setPicture(e.target.value),
+      type: 'file',
+      required: false,
+    },
+  ];
   return (
     <>
       <div>update quantity</div>
+
       <form onSubmit={postData}>
-        <InputProp
-          label="Item Name"
-          type="text"
-          id="item-name"
-          placeholder="Item Name"
-          value={name}
-          onChange={(e: any) => setName(e.target.value)}
-          required={true}
-        />
-        <InputProp
-          label="Brand"
-          type="text"
-          id="Brand"
-          placeholder="Brand"
-          value={brand}
-          onChange={(e: any) => setBrand(e.target.value)}
-          required={true}
-        />
-        <InputProp
-          label="Price"
-          type="number"
-          id="item-price"
-          placeholder="Price"
-          value={price}
-          onChange={(e: any) => setPrice(e.target.value)}
-          required={true}
-        />
-        <InputProp
-          label="Size eg. 1kg, 1L, 1m"
-          type="text"
-          id="item-size"
-          placeholder="Size eg. 1kg, 1L, 1m"
-          value={size}
-          onChange={(e: any) => setSize(e.target.value)}
-          required={true}
-        />
-        <InputProp
-          label="Quantity"
-          type="number"
-          id="item-quantity"
-          placeholder="Quantity"
-          value={quantity}
-          onChange={(e: any) => setQuantity(e.target.value)}
-          required={true}
-        />
-        <InputProp
-          label="Flavor"
-          type="text"
-          id="Flavor"
-          placeholder="Flavor"
-          value={flavor}
-          onChange={(e: any) => setFlavor(e.target.value)}
-          required={true}
-        />
-        <InputProp
-          label="expiry date"
-          type="date"
-          id="expiry-date"
-          placeholder="expiry date"
-          value={expiryDate}
-          onChange={(e: any) => setExpiryDate(e.target.value)}
-          required={true}
-        />
-        <InputProp
-          label="picture"
-          type="file"
-          id="picture"
-          placeholder="picture"
-          value={picture}
-          onChange={(e: any) => setPicture(e.target.value)}
-          required={false}
-        />
+        {dataArray.map((item) => (
+          <InputProp
+            key={item.name}
+            label={item.label}
+            type="text"
+            id={item.name}
+            placeholder={item.label}
+            value={item.value}
+            onChange={item.onChange}
+            required={item.required}
+          />
+        ))}
         <button
           type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="relative inline-flex items-center justify-center p-0.5 m-4 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
         >
-          Add Item
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            Add Item
+          </span>
         </button>
       </form>
     </>
