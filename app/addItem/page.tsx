7196850fad1c993d7512ada1c0e8b541430fd908
anas599 <anas599@yahoo.com>
2023-09-16 22:37:28 +0300

    update form to add new item to inventory

--- app/addItem/page.tsx
+++ app/addItem/page.tsx
@@ -1,13 +1,74 @@
+'use client';
+import { useState, useEffect } from 'react';
+import dateISO8601 from '../functions/dateiso-8601';
+import handle from '@/pages/api/inventory';
 import InputProp from '../component/inputProp';
+import { tr } from 'date-fns/locale';
 const AddForm = () => {
+  const [name, setName] = useState('');
+  const [brand, setBrand] = useState('');
+  const [price, setPrice] = useState('');
+  const [size, setSize] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [flavor, setFlavor] = useState('');
+  const [expiryDate, setExpiryDate] = useState('');
+  const [picture, setPicture] = useState('');
+
+  const handelChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const postData = async (event) => {
+    event.preventDefault(); // Prevent page refresh
+
+    const itemData = {
+      name: name,
+      brand: brand,
+      size: size,
+      price: parseInt(price),
+      quantity: parseInt(quantity),
+      flavor: flavor,
+      expiry: dateISO8601(expiryDate),
+      picture: picture,
+      author: 1, // assuming the author is a user with id 1
+    };
+
+    try {
+      const response = await fetch(
+        `${process.env.DEPLOYDOMAIN}/api/inventory`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(itemData),
+        },
+      );
+      const bodyText = await response.text();
+      try {
+        const json = JSON.parse(bodyText);
+        console.log(json);
+      } catch (error) {
+        console.error(
+          'Received the following instead of valid JSON:',
+          bodyText,
+        );
+      }
+    } catch (e) {
+      console.error('An error occurred while posting', e);
+    }
+  };
   return (
     <>
-      <form>
+      <div>update quantity</div>
+      <form onSubmit={postData}>
         <InputProp
           label="Item Name"
           type="text"
           id="item-name"
           placeholder="Item Name"
+          value={name}
+          onChange={(e: any) => setName(e.target.value)}
           required={true}
         />
         <InputProp
@@ -15,6 +76,8 @@ const AddForm = () => {
           type="text"
           id="Brand"
           placeholder="Brand"
+          value={brand}
+          onChange={(e: any) => setBrand(e.target.value)}
           required={true}
         />
         <InputProp
@@ -22,6 +85,8 @@ const AddForm = () => {
           type="number"
           id="item-price"
           placeholder="Price"
+          value={price}
+          onChange={(e: any) => setPrice(e.target.value)}
           required={true}
         />
         <InputProp
@@ -29,6 +94,8 @@ const AddForm = () => {
           type="text"
           id="item-size"
           placeholder="Size eg. 1kg, 1L, 1m"
+          value={size}
+          onChange={(e: any) => setSize(e.target.value)}
           required={true}
         />
         <InputProp
@@ -36,6 +103,8 @@ const AddForm = () => {
           type="number"
           id="item-quantity"
           placeholder="Quantity"
+          value={quantity}
+          onChange={(e: any) => setQuantity(e.target.value)}
           required={true}
         />
         <InputProp
@@ -43,6 +112,8 @@ const AddForm = () => {
           type="text"
           id="Flavor"
           placeholder="Flavor"
+          value={flavor}
+          onChange={(e: any) => setFlavor(e.target.value)}
           required={true}
         />
         <InputProp
@@ -50,6 +121,8 @@ const AddForm = () => {
           type="date"
           id="expiry-date"
           placeholder="expiry date"
+          value={expiryDate}
+          onChange={(e: any) => setExpiryDate(e.target.value)}
           required={true}
         />
         <InputProp
@@ -57,8 +130,16 @@ const AddForm = () => {
           type="file"
           id="picture"
           placeholder="picture"
+          value={picture}
+          onChange={(e: any) => setPicture(e.target.value)}
           required={false}
         />
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Add Item
+        </button>
       </form>
     </>
   );
