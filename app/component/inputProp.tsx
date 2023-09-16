47f7b4c0505e1b82d79d4ab968380616eefc6bbd
anas599 <anas599@yahoo.com>
2023-09-16 22:36:49 +0300

    update input prop to have value and onchange

--- app/component/inputProp.tsx
+++ app/component/inputProp.tsx
@@ -4,6 +4,8 @@ interface TextInputProps {
   id: string;
   placeholder: string;
   required: boolean;
+  value?: string;
+  onChange?: any;
 }
 
 const InputProp: React.FC<TextInputProps> = ({
@@ -12,16 +14,22 @@ const InputProp: React.FC<TextInputProps> = ({
   id,
   placeholder,
   required,
+  value,
+  onChange,
 }) => {
   return (
     <div className="m-6 mx-10">
       <label
         htmlFor={id}
         className="block mb-1 text-sm font-medium text-gray-200"
-      ></label>
+      >
+        {label}
+      </label>
       <input
         type={type}
         id={id}
+        value={value}
+        onChange={onChange}
         className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
         placeholder={placeholder}
         required={required}
