57ab6c856e1fddc61b005f41a9758ffa8bd3dc14
anas599 <anas599@yahoo.com>
2023-09-19 16:56:42 +0300

    use print button in invoice component

--- app/component/invoice.tsx
+++ app/component/invoice.tsx
@@ -1,6 +1,7 @@
 import { PDFDownloadLink } from '@react-pdf/renderer';
 import { useState, useRef } from 'react';
 import { useGlobalContext } from '../context/store';
+import PrintBtn from './printBtn';
 import { PDFExport } from '@progress/kendo-react-pdf';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -169,14 +170,7 @@ const Invoice = ({ selectedProducts }) => {
               </table>
             </PDFExport>
           </div>
-          <button
-            onClick={handleExportPDF}
-            className="relative inline-flex items-center justify-center p-0.5 m-4 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
-          >
-            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              Print Invoice
-            </span>
-          </button>
+          <PrintBtn handleExportPDF={handleExportPDF} />
         </section>
       </>
     )
