9850ca785d7e8c29ea96c62fbd44074a20680c74
anas599 <anas599@yahoo.com>
2023-09-19 16:56:21 +0300

    add print button reusable compoenet

---
+++ app/component/printBtn.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+interface PrintBtnProps {
+  handleExportPDF: any;
+}
+
+const PrintBtn: React.FC<PrintBtnProps> = ({ handleExportPDF }) => {
+  return (
+    <button
+      onClick={handleExportPDF}
+      className="relative inline-flex items-center justify-center p-0.5 m-4 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
+    >
+      <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+        Print Invoice
+      </span>
+    </button>
+  );
+};
+export default PrintBtn;
