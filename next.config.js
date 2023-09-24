935488b1d2935eb73298c1219abe8266b9d01161
anas599 <anas599@yahoo.com>
2023-09-24 16:51:59 +0300

    add AWS env variables

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   // output: 'export',
   env: {
     DEPLOYDOMAIN: process.env.DEPLOYDOMAIN,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_REGION: process.env.AWS_REGION,
+    AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
   },
   images: {
     domains: [
