9cf643435d4f71991c7047d85fc41751b4c6a138
anas599 <anas599@yahoo.com>
2023-09-25 15:33:12 +0300

    Add upload button component

---
+++ app/component/UploadBtn.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+interface UploadBtnProps {
+  onClick: any;
+  text: string;
+}
+
+const UploadBTN: React.FC<UploadBtnProps> = ({ onClick, text }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="m-6 mx-10 relative inline-flex items-center justify-center p-0.5  overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
+    >
+      <span className="relative flex items-center px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 mr-2" // Added margin-right to separate the icon from the text
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+          />
+        </svg>
+        {text}
+      </span>
+    </button>
+  );
+};
+
+export default UploadBTN;
