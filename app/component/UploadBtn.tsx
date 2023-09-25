8da3993ce87e49375eda45aa5d2d76bb4993e838
anas599 <anas599@yahoo.com>
2023-09-25 16:40:09 +0300

    add type button to prevent confilct with submit

--- app/component/UploadBtn.tsx
+++ app/component/UploadBtn.tsx
@@ -8,6 +8,7 @@ interface UploadBtnProps {
 const UploadBTN: React.FC<UploadBtnProps> = ({ onClick, text }) => {
   return (
     <button
+      type="button"
       onClick={onClick}
       className="m-6 mx-10 relative inline-flex items-center justify-center p-0.5  overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
     >
