7931d71debba48ed4062ef1e71812cb9404d8db6
anas599 <anas599@yahoo.com>
2023-09-26 17:47:42 +0300

    change home page (sale) background colors gradient

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
   return (
     <section>
       <div className="grid grid-cols-3 gap-1">
-        <div className="col-span-2 bg-gray-800 p-4 flex flex-wrap overflow-auto h-screen justify-center">
+        <div className="col-span-2 bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900 p-4 flex flex-wrap overflow-auto h-screen justify-center">
           <ItemPick addToInvoice={addToInvoice} />
         </div>
-        <div className="col-span-1 bg-gray-900 p-4 overflow-auto h-screen w-auto">
+        <div className="col-span-1 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black p-4 overflow-auto h-screen w-auto">
           <Invoice selectedProducts={selectedProducts} />
         </div>
       </div>
