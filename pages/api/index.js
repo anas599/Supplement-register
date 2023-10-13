3be91a44b8a59b7367375d4ea10dac60499067d8
anas599 <anas599@yahoo.com>
2023-10-13 14:21:55 +0300

    use js insted of ts in api

--- pages/api/index.js
+++ pages/api/index.js
@@ -55,19 +55,12 @@
 //   }
 // }
 // export default handler;
-import type { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '@/lib/prisma';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
+async function handler(req, res) {
   if (req.method === 'GET') {
-    try {
-      const inv = await prisma.inventory.findMany({});
-      res.json(inv);
-    } catch (err) {
-      res.status(500).json({ error: 'Something went wrong' });
-    }
+    const oneComment = await prisma.inventory.findMany({});
+    res.status(200).json(oneComment);
   }
 }
+export default handler;
