9795adfc8fb74f1b1b0cd3f7365e20364f67742f
anas599 <anas599@yahoo.com>
2023-10-26 14:08:59 +0300

    test CORS policy api to INABS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export',
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this with your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   env: {
     DEPLOYDOMAIN: process.env.DEPLOYDOMAIN,
     DATABASE_URL: process.env.DATABASE_URL,
@@ -11,18 +32,18 @@ const nextConfig = {
   },
   images: {
     domains: [
-      'olimpsport.com',
-      'upload.wikimedia.org',
-      'tailwindui.com',
-      'images.unsplash.com',
+      "olimpsport.com",
+      "upload.wikimedia.org",
+      "tailwindui.com",
+      "images.unsplash.com",
       `${process.env.BUCKET_NAME}.s3.${process.env.REGION}.amazonaws.com`,
     ],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'olimpsport.com',
-        port: '',
-        pathname: '/eu/**',
+        protocol: "https",
+        hostname: "olimpsport.com",
+        port: "",
+        pathname: "/eu/**",
       },
     ],
   },
