685c6f1dbea8d62c7344bae17ea533b01cbd5efa
anas599 <anas599@yahoo.com>
2023-11-03 15:42:12 +0300

    final form of insta api get and post

--- pages/api/insta/index.ts
+++ pages/api/insta/index.ts
@@ -1,26 +1,48 @@
-import { prisma } from '@/lib/prisma';
+// @ts-nocheck
+   import { prisma } from '@/lib/prisma';
+   import Cors from 'cors';
 
-async function handler(req: any, res: any) {
-  if (req.method === 'GET') {
-    try {
-      const inv = await prisma.insta.findMany({});
-      res.json(inv);
-    } catch (err) {
-      res.status(500).json({ error: 'Something went wrong' });
-    }
-  }
+   // Initialize the cors middleware
+   const cors = Cors({
+     methods: ['GET', 'HEAD', 'POST'],
+   });
 
-if(req.method === 'POST'){
-    try {
-      const {count} = req.body;
-      const inv = await prisma.insta.update({
-        where: {id: 1},
-        data: {count: count}
-      });
-      res.json(inv);
-    } catch (err) {
-      res.status(500).json({ error: 'Something went wrong' });
-    }
-  }
-}
-export default handler;
+   function runMiddleware(req, res, fn) {
+     return new Promise((resolve, reject) => {
+       fn(req, res, (result) => {
+         if (result instanceof Error) {
+           return reject(result);
+         }
+         return resolve(result);
+       });
+     });
+   }
+
+   async function handler(req, res) {
+     // Run the middleware
+     await runMiddleware(req, res, cors);
+
+     if (req.method === 'GET') {
+       try {
+         const inv = await prisma.insta.findMany({});
+         res.json(inv);
+       } catch (err) {
+         res.status(500).json({ error: 'Something went wrong' });
+       }
+     }
+
+     if(req.method === 'POST'){
+       try {
+         const {count} = req.body;
+         const inv = await prisma.insta.update({
+           where: {id: 1},
+           data: {count: count}
+         });
+         res.json(inv);
+       } catch (err) {
+         res.status(500).json({ error: 'Something went wrong' });
+       }
+     }
+   }
+
+   export default handler;
